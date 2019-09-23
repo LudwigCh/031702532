856bb785dc0933ce40c5b2cf1ad0730b4eaf7351
NiftyChen <51016039+NiftyChen@users.noreply.github.com>
2019-09-23 11:11:44 +0800

    Update annotation

--- src/Sudoku/Read_Write.cpp
+++ src/Sudoku/Read_Write.cpp
@@ -14,17 +14,17 @@ int Read::read_judge(int argc, char* argv[])
 {
 	if (argc != 9)
 	{
-		cout << "参数个数错误！";
+		cout << "参数个数错误！";//Error exp:Sudoku.exe -m 9 -n 2 -i input.txt -o 
 		return 1;
 	}
 	if ((argv[2][0]<'3' || argv[2][0]>'9') || (argv[4][0]<'0' || argv[4][0]>'9'))
 	{
-		cout << "输入值不合法！";
+		cout << "输入值不合法！";//Error exp:Sudoku.exe -m cvds -n fd -i input.txt -o output.txt
 		return 1;
 	}
 	if ((argv[1][1] != 'm') || (argv[3][1] != 'n'))
 	{
-		cout << "参数格式错误！";
+		cout << "参数格式错误！";//Error exp:Sudoku.exe -df 9 -n 2 -xbcdf input.txt -o output.txt
 		return 1;
 	}
 	return 0;
@@ -41,7 +41,7 @@ void Read::input(int dimension, int num, string file_in)
 			{
 				infile >> sudo[k][i][j];
 
-				temp[k][i][j] = sudo[k][i][j] - '0';
+				temp[k][i][j] = sudo[k][i][j] - '0';//从"input.txt"读入数独矩阵存入temp
 			}
 
 		}
@@ -49,24 +49,25 @@ void Read::input(int dimension, int num, string file_in)
 }
 bool Read::Check(int n, int key)
 {
-	int col = n % dimen;
-	int	row = n / dimen;
+	int col = n % dimen;//列坐标
+	int	row = n / dimen;//行坐标
 	for (int i = 0; i < dimen; i++)
 	{
-		if (temp[count][row][i] == key)
+		if (temp[count][row][i] == key)//检查行正确性
 			return false;
 	}
 	for (int i = 0; i < dimen; i++)
 	{
-		if (temp[count][i][col] == key)
+		if (temp[count][i][col] == key)//检查列正确性
 			return false;
 	}
 	if (dimen == 3 || dimen == 5 || dimen == 7)
 	{
+		//3，5，7宫格只需判断行与列的合法性
 		return true;
 	}
 	switch (dimen)
-	{
+	{                         //根据所给宫格定义取出当前所在宫格的左上坐标，并遍历宫内格子
 	case 4: {
 		int x = n / 4 / 2 * 2;
 		int y = n % 4 / 2 * 2;
@@ -118,26 +119,15 @@ bool Read::Check(int n, int key)
 	}
 	}
 
-
-
 	return true;
 }
 void Read::equal(int dimension)
 {
-	dimen = dimension;
-	sign = false;
+	dimen = dimension;      //将数独类型值赋给类内变量dimen
+	sign = false;           //计算新的一个数独矩阵时将sign判断符置为false
 }
 void Read::output(string file_out, int num)
 {
-	for (int i = 0; i < dimen; i++)
-	{
-		for (int j = 0; j < dimen; j++)
-		{
-			cout << temp[0][i][j] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
 	ofstream outfile(file_out);
 	for (int k = 0; k < num; k++)
 	{
@@ -152,32 +142,32 @@ void Read::output(string file_out, int num)
 		outfile << "\n";
 	}
 }
-void Read::DFS(int n)
-{
+void Read::DFS(int n)                              //深搜求解，其实就是递归啦。从（0，0）位置开始逐列遍历后再逐行遍历。
+{                                                  //n表示格子的位置，0 <= n <dimen*dimen 
 
 	if (n == dimen * dimen)
-	{
+	{                                              //走完所有格子后标记成功并退出
 		sign = true;
 
 		return;
 	}
-	if (temp[count][n / dimen][n % dimen] != 0)
+	if (temp[count][n / dimen][n % dimen] != 0)    //该位置已有数，跳过
 	{
 		DFS(n + 1);
 	}
 	else
 	{
-		for (int i = 1; i <= dimen; i++)
+		for (int i = 1; i <= dimen; i++)            //对1 - dimen逐个填入验证
 		{
-			if (Check(n, i) == true)
+			if (Check(n, i) == true)             
 			{
-				temp[count][n / dimen][n % dimen] = i;
-				DFS(n + 1);
+				temp[count][n / dimen][n % dimen] = i; //数i在该位置合法，填入
+				DFS(n + 1);                            //继续下一个位置的验证
 				if (sign == true)
 				{
 					return;
 				}
-				temp[count][n / dimen][n % dimen] = 0;
+				temp[count][n / dimen][n % dimen] = 0;//后续无解并返回当前位置，将原先赋值的i清空，尝试下一个数并继续往后搜索
 			}
 		}
 	}
--- src/Sudoku/main.cpp
+++ src/Sudoku/main.cpp
@@ -1,19 +1,19 @@
 #include "Read.h"
 #include<cstdio>
-int dimension;
-int num;
+int dimension;//求解数独类型
+int num;//求解盘面数
 //Sudoku.exe -m 9 -n 2 -i input.txt -o output.txt
 int main(int argc, char* argv[])
 {
 	Read rd;
-	if (rd.read_judge(argc, argv))
+	if (rd.read_judge(argc, argv))//判断命令行参数是否有误
 	{
 		return 0;
-	}
-	dimension = atoi(argv[2]);
-	num = atoi(argv[4]);
-	string file_in = argv[6];
-	string file_out = argv[8];
+	}                                /* 例如 */
+	dimension = atoi(argv[2]);  //9->dimension
+	num = atoi(argv[4]);        //2->num
+	string file_in = argv[6];   //"input.txt"->file_in
+	string file_out = argv[8];  //"ouput.txt"->file_out
 
 	rd.input(dimension, num, file_in);
 	int jj = num;
